b05f3e94ab722c023b1a6ab06b113295436182a0
super_wile_coyote@yahoo.ca <super_wile_coyote@yahoo.ca>
2023-09-18 17:00:53 -0400

    modified:   .github/workflows/github-actions.yml 	modified:   base_external/package/ldd/ldd.mk

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,7 +5,7 @@
 #
 ###############################################################
 
-LDD_VERSION = 6a513e8987c9e4f97d30d56f4422b0c2ad2d84dd
+LDD_VERSION = c83c7290ace77287580b671410030f5a21efb46e
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-doctorterry.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
