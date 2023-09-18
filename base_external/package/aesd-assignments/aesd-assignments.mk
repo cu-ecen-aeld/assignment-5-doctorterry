3b8ff6ea2fbe85e92192cdce2a3dc2529b64ce88
super_wile_coyote@yahoo.ca <super_wile_coyote@yahoo.ca>
2023-09-18 19:10:46 -0400

    modified:   base_external/package/aesd-assignments/aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ###############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '8eb2b7e534cbd60efe79dbfd2f8177d07c5e4cc3'
+AESD_ASSIGNMENTS_VERSION = 8eb2b7e534cbd60efe79dbfd2f8177d07c5e4cc3
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
