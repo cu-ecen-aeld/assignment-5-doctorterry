082f2dfd7dbeecf8b61196ad0a4d486e0ee22cfb
super_wile_coyote@yahoo.ca <super_wile_coyote@yahoo.ca>
2023-09-18 17:44:41 -0400

    modified:   base_external/configs/aesd_qemu_defconfig 	modified:   base_external/external.mk 	modified:   base_external/package/ldd/ldd.mk 	modified:   base_external/rootfs_overlay/etc/init.d/S98lddmodules

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -9,8 +9,8 @@ LDD_VERSION = c83c7290ace77287580b671410030f5a21efb46e
 LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-doctorterry.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
-
-LDD_MODULE_SUBDIRS = misc-modules scull
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
