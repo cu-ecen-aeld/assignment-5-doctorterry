cd290e9a65d604486d239cf9d8b8ef19ea1a4454
super_wile_coyote@yahoo.ca <super_wile_coyote@yahoo.ca>
2023-09-24 18:17:16 -0400

    modified:   base_external/package/aesd-assignments/aesd-assignments.mk 	modified:   base_external/package/aesd-char-device/Config.in 	modified:   base_external/package/aesd-char-device/aesd-char-device.mk 	modified:   base_external/rootfs_overlay/etc/init.d/S97aesdcharmodule 	modified:   base_external/rootfs_overlay/etc/init.d/S98lddmodules

--- base_external/package/aesd-char-device/aesd-char-device.mk
+++ base_external/package/aesd-char-device/aesd-char-device.mk
@@ -5,8 +5,8 @@
 #
 ##############################################################
 
-AESD_CHAR_DEVICE_VERSION = 8471cdf23c496bdac66e500bfc5fdf584ecdc468
-AESD_CHAR_DEVICE_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-RA251995.git
+AESD_CHAR_DEVICE_VERSION = 44e2b8f8d673e551cad592e21e9d2037348498fa
+AESD_CHAR_DEVICE_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-doctorterry.git
 AESD_CHAR_DEVICE_SITE_METHOD = git
 AESD_CHAR_DEVICE_GIT_SUBMODULES = YES
 AESD_CHAR_DEVICE_MODULE_SUBDIRS = aesd-char-driver
